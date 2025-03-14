1c776860c445916088c9bda7b6cb60e8d07185f7
Tanmay0Bhosale <113248930+Tanmay0Bhosale@users.noreply.github.com>
2025-03-14 19:43:46 +0530

    before edit feature

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+const fs= require('fs');
 const express = require('express');
 const path = require('path');
+const { log } = require('console');
 const app =express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public'))) ;
 app.get('/', function(req,res){
-    res.render("index");
+    fs.readdir('./files', function(err,files){
+        res.render("index" , {files: files});
+    });
 });
-
+app.post('/create', function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, `${req.body.details}`, function(err){
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+app.get('/files/:filename', function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render("files", {filename: req.params.filename, filedata: filedata});
+    });
+});
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
